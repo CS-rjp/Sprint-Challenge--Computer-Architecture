7b8f9a298bac42180d84f24c74bc506e82fcac4f
Rebecca J Proctor <67437806+jproctor-rebecca@users.noreply.github.com>
2021-03-19 13:14:29 -0500

    Add files via upload

--- ls8/ls8.py
+++ ls8/ls8.py
@@ -3,9 +3,16 @@
 """Main."""
 
 import sys
-from cpu import *
+from cpu import CPU
+
+if len(sys.argv) !=2:
+    print(f"{sys.argv[0]} <file name>")
+    sys.exit(1)
 
 cpu = CPU()
 
-cpu.load()
+cpu.load(sys.argv[1])
 cpu.run()
+
+if __name__ == "__main__":
+    pass
